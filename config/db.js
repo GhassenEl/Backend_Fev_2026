ada2410ba676b3acaa48197f0bb9efcea2665b5b
Ghassen El Jezi <eljezi.ghassen@esprit.tn>
2026-01-31 20:18:37 +0100

    app

---
+++ config/db.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose');
+module.exports.connectToMongoDB=
+mongoose.connect(process.env.url_MongoDB)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB', err);
+    });
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json('respond with a ressource');
 });
-res.json('index');
+
 
 module.exports = router;
