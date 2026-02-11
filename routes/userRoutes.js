b3871f8180c1d426e6e90615a585a81ca65a068e
Raslen korbosli <raslen.korbosli01@gmail.com>
2026-02-11 21:59:56 +0100

    update user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,17 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const userController = require("../controllers/userController");
+const userController = require('../controllers/userController');
 
 // Routes CRUD
-router.post("/", userController.createUser); //create
-router.get("/", userController.getAllUsers); //read all
-router.get("/:id", userController.getUserById); //read one
-router.put("/:id", userController.updateUser); //update
-router.delete("/:id", userController.deleteUser); //suppression
+router.post('/getAllUsers', userController.createUser); //create
+//pour tester
+router.get('/a', async (req, res) => {
+  res
+    .status(200)
+    .json({ status: 'success', message: 'All users retrieved successfully' });
+}); //read all
+router.get('/:id', userController.getUserById); //read one
+router.put('/:id', userController.updateUser); //update
+router.delete('/:id', userController.deleteUser); //suppression
 
 module.exports = router;
