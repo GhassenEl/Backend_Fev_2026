0871910eb91501e81ccbe07ffa41bb281ab8c42a
Ghassen El Jezi <eljezi.ghassen@esprit.tn>
2026-02-26 11:28:24 +0100

    app.js

--- app.js
+++ app.js
@@ -1,50 +1,87 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const http = require('http'); //1
-
-const { connectToMongoDB } = require('./config/db');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users.routes');
-var osRouter = require('./routes/os.routes');
-var factureRouter = require('./routes/facture.routres');
-
-require('dotenv').config(); // Load environment variables
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const http = require("http");
+const { connectToMongoDB } = require("./config/db");
+require("dotenv").config();
+// Import des routes basées sur vos modèles (sans index)
+var usersRouter = require("./routes/users.routes");
+var animalRouter = require("./routes/animal.routes");
+var clientRouter = require("./routes/client.routes");
+var commandeRouter = require("./routes/commande.routes");
+var evaluationRouter = require("/routes/evaluation.routes");
+var livreurRouter = require("/routes/livreur.routes");
+var panierRouter = require("/routes/panier.routes");
+var produitRouter = require("/routes/produit.routes");
+const { error } = require("console");
+const { connect } = require("http2");
 
 var app = express();
 
-app.use(logger('dev'));
+// Middleware
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/index', indexRouter);
-app.use('/users', usersRouter);
-app.use('/os', osRouter);
-app.use('/facture', factureRouter);
+// Routes
+app.use("/users", usersRouter);
+app.use("/animaux", animalRouter);
+app.use("/clients", clientRouter);
+app.use("/commandes", commandeRouter);
+app.use("/evaluations", evaluationRouter);
+app.use("/livreurs", livreurRouter);
+app.use("/paniers", panierRouter);
+app.use("/produits", produitRouter);
 
+// Route racine (optionnelle)
+app.get("/", (req, res) => {
+  res.json({
+    message: "Bienvenue sur l'API PFE 2026",
+    endpoints: [
+      "/users",
+      "/animaux",
+      "/clients",
+      "/commandes",
+      "/evaluations",
+      "/livreurs",
+      "/paniers",
+      "/produits",
+    ],
+  });
+});
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
-
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
-  res.json('error');
+  res.json({
+    error: err.message,
+    status: err.status || 500,
+  });
 });
 
-//2
+// Création et démarrage du serveur
 const server = http.createServer(app);
-server.listen(process.env.PORT, () => {
-  connectToMongoDB();
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  connectToMangoDB();
+  console.log(" Serveur démarré  sur  http://localhost:${PORT}");
+  console.log(" Routes disponibles:");
+  console.log("  http://localhost:${PORT}");
+  console.log("  http://localhost:${PORT}/users");
+  console.log("  http://localhost:${PORT}/animaux");
+  console.log("  http://localhost:${PORT}/clients");
+  console.log("  http://localhost:${PORT}/commandes");
+  console.log("  http://localhost:${PORT}/evaluations");
+  console.log("  http://localhost:${PORT}/livreurs");
+  console.log("  http://localhost:${PORT}/paniers");
+  console.log("  http://localhost:${PORT}/produits");
 });
